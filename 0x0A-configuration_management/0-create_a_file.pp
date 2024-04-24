11e96f25e16de7d63e90f403992bfe8105f24104
RorisangMoeng <moengqueen@gmail.com>
2024-04-24 13:19:34 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #The code will create a file new school inside the /tmp directory
 file { */tmp/school:
 	ensure => file,
-	content => 'I love Puppet'
-	mode => '07744'
-	owner => 'www-data'
-	group => 'www-data'
+	content => 'I love Puppet',
+	mode => '07744',
+	owner => 'www-data',
+	group => 'www-data',
 }
